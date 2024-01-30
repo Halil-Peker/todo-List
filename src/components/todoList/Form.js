246789d0e8df567d61d94ee9c6227506d4fdc889
Halil-Peker <64087736+Halil-Peker@users.noreply.github.com>
2024-01-30 18:42:37 +0300

    Edit
    kod düzenlendi

--- src/components/todoList/Form.js
+++ src/components/todoList/Form.js
@@ -1,32 +1,23 @@
-import React from 'react'
+import React from "react";
 
-function Form({setItems, items}) {
-    const formSubmit =(e)=>{
-        e.preventDefault();
-        setItems([...items, {mission:e.target.input.value, completed:false }]);
-        e.target.input.value = "";
-    }
+function Form({ setItems, items }) {
+  const formSubmit = (e) => {
+    e.preventDefault();
+    setItems([...items, { mission: e.target.input.value, completed: false }]);
+    e.target.input.value = "";
+  };
 
   return (
-     <div className='form'>
-    <form  onSubmit={formSubmit}>
-
-        {/* <input 
-        onChange={(e)=> {const newItem=[...items];
-        newItem.map((item)=>(item.completed = e.target.checked))
-        setItems(newItem);
-        }}  type="checkbox" /> */}
-        
-        <input className="input"
+    <div className="form">
+      <form onSubmit={formSubmit}>
+        <input
+          className="input"
           name="input"
-          placeholder="What needs to be done?"/>
-
-
-
-    </form>
-        
+          placeholder="What needs to be done?"
+        />
+      </form>
     </div>
-  )
+  );
 }
 
-export default Form
+export default Form;
--- src/components/todoList/FormList.js
+++ src/components/todoList/FormList.js
@@ -1,68 +1,64 @@
-import {useState} from 'react'
+import { useState } from "react";
 import "../../App.css";
 
 function FormList({ items, setItems }) {
+  const [filter, setFilter] = useState("all");
 
-    const [filter, setFilter] = useState("all");
+  // Görev tamamlandı ya da tamamlanmadı için tik atma yeri
+  const toggleComplete = (i) => {
+    const newItems = [...items];
+    newItems[i].completed = !newItems[i].completed;
+    setItems(newItems);
+  };
 
-    // Görev tamamlandı ya da tamamlanmadı için tik atma yeri
-    const toggleComplete = (i) => {
-        const newItems = [...items];
-        newItems[i].completed = !newItems[i].completed;
-        setItems(newItems);
-      };
+  // Seçilen görevi silme
+  const deleteSection = (i) => {
+    const newItems = [...items];
+    newItems.splice(i, 1);
+    setItems(newItems);
+  };
 
-    // Seçilen görevi silme
-      const deleteSection= (i)=>{
-        const newItems =[...items];
-        newItems.splice(i , 1);
-        setItems(newItems);
+  // Tamamlananları silme
+  const deleteComplete = () => {
+    const newItems = [];
+    items.forEach((item) => {
+      if (!item.completed) {
+        newItems.push(item);
       }
+    });
+    setItems(newItems);
+  };
 
-        const deleteComplete=()=>{
-          const newItems= [];
-          items.forEach((item)=>{
-            if(!item.completed){
-              newItems.push(item)
-            }
-          })
-          setItems(newItems)
-        }
-
-    // Görev kartının alt kısmında yer alan "all, active ve completed işlemlerinin döndüğü kısım"
-    const filteredItem= items.filter((item)=>{
-        if(filter === "all"){
-            return true;
-        }
-        else if(filter === "completed"){
-            return item.completed
-        }
-        else return !item.completed
-    })
+  // Görev kartının alt kısmında yer alan "all, active ve completed işlemlerinin döndüğü kısım"
+  const filteredItem = items.filter((item) => {
+    if (filter === "all") {
+      return true;
+    } else if (filter === "completed") {
+      return item.completed;
+    } else return !item.completed;
+  });
   return (
     <div className="list">
-        <div>
-            <ul>
-                {filteredItem.map((item, i) => (
-                    <li key={i}>
-                         <input
+      <div>
+        <ul>
+          {filteredItem.map((item, i) => (
+            <li key={i}>
+              <input
                 className="chckBtn"
                 type="checkbox"
                 checked={item.completed}
                 onChange={() => toggleComplete(i)}
               />
-                        <span className="todo">{item.mission}</span>
-                        
+              <span className="todo">{item.mission}</span>
+
               <button className="dltBtn" onClick={() => deleteSection(i)}>
                 Delete
               </button>
-                    </li>
-                    
-                ))}
-                
-            </ul>
-        </div>
-        <div className="buttons">
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="buttons">
         <span className="count">{items.length} left</span>
         <button onClick={() => setFilter("all")}>All Todos</button>
         <button onClick={() => setFilter("completed")}>Done Todos</button>
@@ -70,8 +66,7 @@ function FormList({ items, setItems }) {
         <button onClick={() => deleteComplete()}>clear completed</button>
       </div>
     </div>
-    
-  )
+  );
 }
 
-export default FormList
+export default FormList;
